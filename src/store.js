311d2e557aa2cf3e8b8969b26bffec3ce6aad819
Joe Blank <joe.blank@live.com>
2017-12-07 11:54:54 -0700

    cleaned up some code

--- src/store.js
+++ src/store.js
@@ -2,4 +2,4 @@ import { createStore, applyMiddleware } from 'redux';
 import reducer from './ducks/user';
 import promiseMiddleware from 'redux-promise-middleware';
 
-export default createStore(reducer, {}, applyMiddleware(promiseMiddleware()));
+export default createStore(reducer, applyMiddleware(promiseMiddleware()));
